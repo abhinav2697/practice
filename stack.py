d7406ad80523727744a93ac8a3a9920e4007b58e
Abhinav Kulkarni <abhinav2697@gmail.com>
2021-07-10 00:47:51 +0530

    added line

--- stack.py
+++ stack.py
@@ -1,1 +1,11 @@
 print("LIFO")
+print("(stack)")
+#write a program to print even numbers
+def even(n):
+    if n%2==0:
+        print("even")
+    else:print("odd")
+print(even(10))
+
+#write a program to print prime numbers
+def prime():
